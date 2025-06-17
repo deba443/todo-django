a2db82b646e51c164685b5aa27f5500896d3f970
deba443 <debaprasad443@gmail.com>
2025-06-17 16:46:20 +0530

    Complete CBV for crud also

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import todo_views
 
 urlpatterns = [
-    path('crud/',todo_views.todo_manager)
+    # path('crud/',todo_views.todo_manager)
+    path('crud/',todo_views.MyModelApiView.as_view()),
+    path('crud/<int:pk>/',todo_views.MyModelApiView.as_view())
 ]
--- todoapp/views/todo_views.py
+++ todoapp/views/todo_views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from ..serializers import task_serializers
 from ..models import todo
+from rest_framework.views import APIView
 
 
 
@@ -26,4 +27,64 @@ def todo_manager_crud(request,pk):
     except todo.Todo.DoesNotExist:
         return Response({'error':"Task Not found"},status=status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
-        serializer=task_serializers.Taskserializers()
+        serializer=task_serializers.Taskserializers(task)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer=task_serializers.Taskserializers(task,data=request.data)
+        if serializer.is_valid():
+            serializer.save()   
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        task.delete()
+        return Response({'message':"deleted"},status=status.HTTP_204_NO_CONTENT)
+    
+class MyModelApiView(APIView):
+    def get_object(self,pk):
+        try:
+            item=todo.Todo.objects.get(pk=pk)
+            if not item.exists():
+                return Response({"ereor":"Item not found"},status=status.HTTP_404_NOT_FOUND)
+            serializer=task_serializers.Taskserializers(item)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error":str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    def get(self,request,pk=None):
+        try:
+            tasks=todo.Todo.objects.all()
+            if not tasks.exists():
+                return Response({"error":"Task Not found"},status=status.HTTP_404_NOT_FOUND)
+            serializer=task_serializers.Taskserializers(tasks,many=True)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error":str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    def post(self,requset):
+        try:
+            seralizer=task_serializers.Taskserializers(data=requset.data)
+            if seralizer.is_valid():
+                seralizer.save()
+                return Response(seralizer.data,status=status.HTTP_201_CREATED)
+            return Response(seralizer.errors,status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error":str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    def put(self,request,pk):
+        try:
+            task=todo.Todo.objects.get(pk=pk)
+            if not task.exists():
+                return Response({"error":"tat key item doesnot exist in db"},status=status.HTTP_404_NOT_FOUND)
+            serializer=task_serializers.Taskserializers(task,data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_200_OK)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error":str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    def delete(self,request,pk):
+        try:
+            item=todo.Todo.objects.get(pk=pk)
+            if not item.exists():
+                return Response({"error":"tat key item doesnot exist in db"},status=status.HTTP_404_NOT_FOUND)
+            item.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Exception as e:
+            return Response({"error":str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
